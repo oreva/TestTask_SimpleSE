bf9e8b13fd080544207097bda0176a9afa09523c
Olga Reva <reva.olga@gmail.com>
2018-05-04 17:26:24 +0300

    Search Engine Server application added. First simple version.

---
+++ search-engine-server/README.md
@@ -0,0 +1,6 @@
+# Search Engine Server #
+
+Server application.
+
+Call "mvn clean install spring-boot:run" command to build and run this application.
+
---
+++ search-engine-server/src/main/java/com/conductor/testtask/searchengine/server/internalapi/ApiContext.java
@@ -0,0 +1,8 @@
+package com.conductor.testtask.searchengine.server.internalapi;
+
+public class ApiContext {
+
+    public static final String ROOT_API_PATH = "/api";
+    public static final String INTERNAL_ROOT_API_PATH = "/rest";
+
+}
